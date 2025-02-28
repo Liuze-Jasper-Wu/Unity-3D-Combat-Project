5cd7eb55da7de51f874beb22a159b47d48bf5b08
JasperOnCode <440052693@gapps.yrdsb.ca>
2025-02-27 21:00:23 -0500

    new project with slashes

--- Assets/Scripts/Character/ThirdPersonController.cs
+++ Assets/Scripts/Character/ThirdPersonController.cs
@@ -81,8 +81,7 @@ namespace StarterAssets
 
         // player
         private float _speed;
-        private float _motionSpeed;
-        private float _actualSpeed;
+        private float _combatSpeed;                                       
         private float _animationBlend;
         private float _targetRotation = 0.0f;
         private float _rotationVelocity;
@@ -95,6 +94,7 @@ namespace StarterAssets
 
         // animation IDs
         private int _animIDSpeed;
+        private int _animIDCombatSpeed;
         private int _animIDGrounded;
         private int _animIDJump;
         private int _animIDFreeFall;
@@ -173,6 +173,7 @@ namespace StarterAssets
         private void AssignAnimationIDs()
         {
             _animIDSpeed = Animator.StringToHash("Speed");
+            _animIDCombatSpeed = Animator.StringToHash("CombatSpeed");
             _animIDGrounded = Animator.StringToHash("Grounded");
             _animIDJump = Animator.StringToHash("Jump");
             _animIDFreeFall = Animator.StringToHash("FreeFall");
@@ -217,77 +218,143 @@ namespace StarterAssets
 
         private void Move()
         {
-            if (!_weaponController.isWeaponDrawn)
+            if (!_weaponController._isAttacking)
             {
-                _actualSpeed = _speed;
-            }
-            else
-            {
-                _actualSpeed = _motionSpeed;
-            }
-            // set/target speed based on move speed, sprint speed and if sprint is pressed
-            float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
+                if (!_weaponController._isWeaponDrawn)
+                {
+                    // set/target speed based on move speed, sprint speed and if sprint is pressed
+                    float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
 
-            // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
+                    // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
 
-            // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-            // if there is no input, set the target speed to 0
-            if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+                    // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+                    // if there is no input, set the target speed to 0
+                    if (_input.move == Vector2.zero) targetSpeed = 0.0f;
 
-            // a reference to the players current horizontal velocity
-            float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+                    // a reference to the players current horizontal velocity
+                    float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
 
-            float speedOffset = 0.1f;
-            float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
+                    float speedOffset = 0.1f;
+                    float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
 
-            // accelerate or decelerate to target speed
-            if (currentHorizontalSpeed < targetSpeed - speedOffset ||
-                currentHorizontalSpeed > targetSpeed + speedOffset)
-            {
-                // creates curved result rather than a linear one giving a more organic speed change
-                // note T in Lerp is clamped, so we don't need to clamp our speed
-                _actualSpeed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
-                    Time.deltaTime * SpeedChangeRate);
+                    // accelerate or decelerate to target speed
+                    if (currentHorizontalSpeed < targetSpeed - speedOffset ||
+                        currentHorizontalSpeed > targetSpeed + speedOffset)
+                    {
+                        // creates curved result rather than a linear one giving a more organic speed change
+                        // note T in Lerp is clamped, so we don't need to clamp our speed
+                        _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
+                            Time.deltaTime * SpeedChangeRate);
 
-                // round speed to 3 decimal places
-                _actualSpeed = Mathf.Round(_actualSpeed * 1000f) / 1000f;
-            }
-            else
-            {
-                _actualSpeed = targetSpeed;
-            }
+                        // round speed to 3 decimal places
+                        _speed = Mathf.Round(_speed * 1000f) / 1000f;
+                    }
+                    else
+                    {
+                        _speed = targetSpeed;
+                    }
 
-            _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
-            if (_animationBlend < 0.01f) _animationBlend = 0f;
+                    _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+                    if (_animationBlend < 0.01f) _animationBlend = 0f;
 
-            // normalise input direction
-            Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+                    // normalise input direction
+                    Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
 
-            // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-            // if there is a move input rotate player when the player is moving
-            if (_input.move != Vector2.zero)
-            {
-                _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
-                                  _mainCamera.transform.eulerAngles.y;
-                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
-                    RotationSmoothTime);
+                    // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+                    // if there is a move input rotate player when the player is moving
+                    if (_input.move != Vector2.zero)
+                    {
+                        _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
+                                          _mainCamera.transform.eulerAngles.y;
+                        float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
+                            RotationSmoothTime);
 
-                // rotate to face input direction relative to camera position
-                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
-            }
+                        // rotate to face input direction relative to camera position
+                        transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+                    }
 
 
-            Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+                    Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
 
-            // move the player
-            _controller.Move(targetDirection.normalized * (_actualSpeed * Time.deltaTime) +
-                             new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+                    // move the player
+                    _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
+                                     new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
 
-            // update animator if using character
-            if (_hasAnimator)
-            {
-                _animator.SetFloat(_animIDSpeed, _animationBlend);
-                _animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
+                    // update animator if using character
+                    if (_hasAnimator)
+                    {
+                        Debug.Log("animation Blend(Combat, speed): " + _animationBlend);
+                        _animator.SetFloat(_animIDSpeed, _animationBlend);
+                        _animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
+                    }
+                }
+                else
+                {
+                    // set/target speed based on move speed, sprint speed and if sprint is pressed
+                    float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
+
+                    // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
+
+                    // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+                    // if there is no input, set the target speed to 0
+                    if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+
+                    // a reference to the players current horizontal velocity
+                    float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+
+                    float speedOffset = 0.1f;
+                    float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
+
+                    // accelerate or decelerate to target speed
+                    if (currentHorizontalSpeed < targetSpeed - speedOffset ||
+                        currentHorizontalSpeed > targetSpeed + speedOffset)
+                    {
+                        // creates curved result rather than a linear one giving a more organic speed change
+                        // note T in Lerp is clamped, so we don't need to clamp our speed
+                        _combatSpeed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
+                            Time.deltaTime * SpeedChangeRate);
+
+                        // round speed to 3 decimal places
+                        _combatSpeed = Mathf.Round(_combatSpeed * 1000f) / 1000f;
+                    }
+                    else
+                    {
+                        _combatSpeed = targetSpeed;
+                    }
+
+                    _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+                    if (_animationBlend < 0.01f) _animationBlend = 0f;
+
+                    // normalise input direction
+                    Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+
+                    // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+                    // if there is a move input rotate player when the player is moving
+                    if (_input.move != Vector2.zero)
+                    {
+                        _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
+                                          _mainCamera.transform.eulerAngles.y;
+                        float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
+                            RotationSmoothTime);
+
+                        // rotate to face input direction relative to camera position
+                        transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+                    }
+
+
+                    Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+                    // move the player
+                    _controller.Move(targetDirection.normalized * (_combatSpeed * Time.deltaTime) +
+                                     new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+                    // update animator if using character
+                    if (_hasAnimator)
+                    {
+                        _animator.SetFloat(_animIDCombatSpeed, _animationBlend);
+                        _animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
+                    }
+                }
             }
         }
 
--- Assets/Scripts/Character/WeaponController.cs
+++ Assets/Scripts/Character/WeaponController.cs
@@ -3,54 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class WeaponController : MonoBehaviour
+namespace StarterAssets
 {
-    // Reference to the input action
-    public InputAction drawWeaponAction;
-    public Animator anim;
-    private bool _isWeaponDrawn = false;
 
-    private void OnEnable()
+    public class WeaponController : MonoBehaviour
     {
-        // Enable the DrawWeapon input action when the object is enabled
-        drawWeaponAction.Enable();
-    }
+        // Reference to the input action
+        public InputAction drawWeaponAction;
+        public InputAction attackAction;
+        public Animator anim;
+        public bool _isWeaponDrawn = false;
+        public bool _isAttacking = false;
 
-    private void OnDisable()
-    {
-        // Disable the DrawWeapon input action when the object is disabled
-        drawWeaponAction.Disable();
-    }
-
-    private void Update()
-    {
-        // Check if the input action is triggered (e.g., the R key)
-        if (drawWeaponAction.triggered)
+        private void OnEnable()
         {
-            OnDrawWeapon();
+            // Enable the DrawWeapon input action when the object is enabled
+            drawWeaponAction.Enable();
+            attackAction.Enable();
         }
-    }
 
-    // Method to be called when the weapon is drawn
-    private void OnDrawWeapon()
-    {
-        if (!_isWeaponDrawn)
+        private void OnDisable()
         {
-            // Trigger weapon drawing logic
-            Debug.Log("Weapon Drawn");
-            _isWeaponDrawn = true;
-
-            // Add weapon draw logic (e.g., animation, enable weapon)
-            anim.SetTrigger("DrawWeapon");
+            // Disable the DrawWeapon input action when the object is disabled
+            drawWeaponAction.Disable();
+            attackAction.Disable();
         }
-        else
-        {
-            // Trigger weapon sheathing logic
-            Debug.Log("Weapon Sheathed");
-            _isWeaponDrawn = false;
 
-            // Add weapon sheathing logic (e.g., animation, disable weapon)
-            anim.SetTrigger("SheathWeapon");
+        private void Update()
+        {
+            // Check if the input action is triggered (e.g., the R key)
+            if (drawWeaponAction.triggered)
+            {
+                OnDrawWeapon();
+            }
+            else if(attackAction.triggered)
+            {
+                OnAttack();
+            }
+        }
+
+        // Method to be called when the weapon is drawn
+        private void OnDrawWeapon()
+        {
+            if (!_isWeaponDrawn)
+            {
+                // Trigger weapon drawing logic
+                Debug.Log("Weapon Drawn");
+                _isWeaponDrawn = true;
+
+                // Add weapon draw logic (e.g., animation, enable weapon)
+                anim.SetTrigger("DrawWeapon");
+            }
+            else
+            {
+                // Trigger weapon sheathing logic
+                Debug.Log("Weapon Sheathed");
+                _isWeaponDrawn = false;
+
+                // Add weapon sheathing logic (e.g., animation, disable weapon)
+                anim.SetTrigger("SheathWeapon");
+            }
+        }
+
+        // Method to be called when the weapon is drawn
+        private void OnAttack()
+        {
+            if (_isWeaponDrawn)
+            {
+                // Trigger weapon drawing logic
+                Debug.Log("Weapon Drawn");
+                _isAttacking = true;
+
+                // Add weapon draw logic (e.g., animation, enable weapon)
+                anim.SetTrigger("Attack");
+                _isAttacking = false;
+            }
         }
     }
 }
